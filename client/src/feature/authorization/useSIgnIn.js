6a024badba0934b48bf05960a47696f029b327e9
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-11 16:42:05 +0330

    add query client in use login and use sign in

--- client/src/feature/authorization/useSIgnIn.js
+++ client/src/feature/authorization/useSIgnIn.js
@@ -1,10 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signIn as signInAPI } from "../../service/apiAuth";
 
 export function useSignIn() {
+  const queryClient = useQueryClient();
   const { mutate: signIn, isPending } = useMutation({
     mutationFn: ({ userName, email, password, role }) =>
       signInAPI({ userName, password, email, role }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (err) => {
+      console.log(err);
+    },
   });
   return { signIn, isPending };
 }
--- client/src/feature/useLogin.js
+++ client/src/feature/useLogin.js
@@ -1,9 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginAPI } from "../service/apiAuth";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+    onError: (err) => {
+      console.log(err);
+    },
   });
   return { login, isPending };
 }
