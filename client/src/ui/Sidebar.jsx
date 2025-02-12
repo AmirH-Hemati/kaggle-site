c30a3b62d91f3336f2c7b8c358c46d495ea81480
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-12 22:29:31 +0330

    add IsActive in navlink for get style in sidebar

--- client/src/ui/Sidebar.jsx
+++ client/src/ui/Sidebar.jsx
@@ -2,9 +2,17 @@ import { Data, DocumentUpload } from "iconsax-react";
 import { NavLink } from "react-router-dom";
 
 function Sidebar() {
+  //""
   return (
-    <aside className="w-[20%] flex flex-col gap-6 border-l-2 border-black/20 py-4 ">
-      <NavLink className="border-l-2 flex items-center gap-2 rounded-sm p-3 text-sm font-semibold hover:bg-[#F8F9FA]">
+    <aside className="w-[20%] flex flex-col gap-6 border-l-2 border-black/20 py-8 ">
+      <NavLink
+        to={`/datasets`}
+        className={({ isActive }) =>
+          `flex items-center gap-2 rounded-sm p-3 text-sm font-semibold hover:bg-[#F8F9FA]     ${
+            isActive ? "border-l-2 border-black bg-[#F8F9FA] rounded-none" : "border-l-0 bg-white "
+          }`
+        }
+      >
         <DocumentUpload size="26" color="black" />
         <p>اپلود فایل</p>
       </NavLink>
