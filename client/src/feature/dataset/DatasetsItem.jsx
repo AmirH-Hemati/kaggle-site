aa49814354bc34efada0fcefd3d34c80db995818
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-12 11:02:27 +0330

    create datasets item

---
+++ client/src/feature/dataset/DatasetsItem.jsx
@@ -0,0 +1,18 @@
+function DatasetsItem() {
+  return (
+    <li className="flex h-full rounded-3xl shadow-md hover:shadow-2xl border-2 border-black/30">
+      <img
+        src=""
+        alt=""
+        className="rounded-3xl w-full object-cover aspect-square"
+      />
+      <p>Car Theft Data in India</p>
+
+      <p>amirHossen hemati</p>
+      <p>uploadedAt : 9 days ago</p>
+      <p>1file json</p>
+    </li>
+  );
+}
+
+export default DatasetsItem;
