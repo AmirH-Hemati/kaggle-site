ed3a0a4a57bb850cc3aa98542647544b8e8a7d67
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-12 11:14:27 +0330

    change name to title in upload file

--- client/src/page/UploadFile.jsx
+++ client/src/page/UploadFile.jsx
@@ -6,14 +6,14 @@ function UploadFile() {
     e.preventDefault();
     const formData = new FormData();
     console.log(e.target.file);
-    formData.append("name", e.target.name.value);
+    formData.append("title", e.target.title.value);
     formData.append("description", e.target.description.value);
     formData.append("file", e.target.file.files[0]);
     createFile(formData);
   }
   return (
     <form onSubmit={handelCreateFile} className="flex flex-col ">
-      <input type="text" placeholder="name file" name="name" />
+      <input type="text" placeholder="title file" name="title" />
       <input type="text" placeholder="description" name="description" />
       <input type="file" name="file" />
       <button>submit</button>
