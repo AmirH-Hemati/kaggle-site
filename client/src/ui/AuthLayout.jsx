de25e6e2c663d1585b1d1c3a5672bfac532b5a8d
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-02-18 11:46:06 +0330

    to fix bug

--- client/src/ui/AuthLayout.jsx
+++ client/src/ui/AuthLayout.jsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function AuthLayout() {
-  return <div className="w-full h-full flex  justify-center items-center">
-    <Outlet />
-  </div>;
+  const [hasLogin, setHasLogin] = useState(false);
+  return (
+    <div className="w-full h-full flex flex-col  justify-center items-center">
+      <Outlet />
+      <Link
+        to={hasLogin ? `/login` : `/signIn`}
+        onClick={() => setHasLogin((hasLogin) => !hasLogin)}
+      >
+        {hasLogin ? " اکانت دارید ؟ وارد شوید" : "اکانت ندارید ؟ ثبت نام کنید"}
+      </Link>
+    </div>
+  );
 }
 
 export default AuthLayout;
