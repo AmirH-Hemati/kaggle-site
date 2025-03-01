b8f96812243da0efdf62d13defc949326fa4658a
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-03-01 22:25:09 +0330

    create component for userSidebar

--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import { useCurrentUser } from "../feature/authorization/useCurrentUser";
+import { Icon, User } from "iconsax-react";
+import { useState } from "react";
+import UserSidebar from "./UserSidebar";
 
 function Header() {
   const { currentUser } = useCurrentUser();
+  const [isOpen, setIsOpen] = useState(false);
   console.log(currentUser?.data);
   return (
     <header className="w-full bg-white shadow-md z-30">
@@ -31,12 +35,17 @@ function Header() {
             </Link>
           )}
         </nav>
-        <Link to="/login">
-          <Button extraStyle="w-24 py-2" type="contained">
-            ورود
-          </Button>
-        </Link>
+        {currentUser?.data ? (
+          <User onClick={() => setIsOpen((isOpen) => !isOpen)} />
+        ) : (
+          <Link to="/login">
+            <Button extraStyle="w-24 py-2" type="contained">
+              ورود
+            </Button>
+          </Link>
+        )}
       </div>
+      {isOpen && <UserSidebar isOpen={isOpen} setIsOpen={setIsOpen} />}
     </header>
   );
 }
---
+++ client/src/ui/UserSidebar.jsx
@@ -0,0 +1,34 @@
+import { CloseSquare } from "iconsax-react";
+import { Link } from "react-router-dom";
+import Button from "./Button";
+
+function UserSidebar({ setIsOpen, isOpen }) {
+  return (
+    <div
+      className={`fixed top-0 right-0 h-full w-1/5 bg-white shadow-lg  transition-transform ease-in-out duration-500  ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="flex justify-between items-center p-4 border-b">
+        <h2 className="text-lg font-semibold">منوی کاربر</h2>
+        <button onClick={() => setIsOpen(false)} className="text-2xl">
+          <CloseSquare size="32" color="black" className="cursor-pointer" />
+        </button>
+      </div>
+
+      <nav className="p-4 space-y-4 flex flex-col items-start">
+        <Link to="/profile" className="w-full  p-2 hover:bg-gray-200 rounded">
+          پروفایل من
+        </Link>
+        <Link to="/settings" className="w-full  p-2 hover:bg-gray-200 rounded">
+          تنظیمات
+        </Link>
+        <Button type={`primary`} extraStyle={`w-full`}>
+          خروج
+        </Button>
+      </nav>
+    </div>
+  );
+}
+
+export default UserSidebar;
