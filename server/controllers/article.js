bba5fdfb882593090c5f6393a76c085fd503e479
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-03-07 20:24:46 +0330

    createa api for article such as delete or get all article or update article or get single article informtaion

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -1,5 +1,44 @@
-export async function getAllArticle(req, res) {}
-export async function getArticle(req, res) {}
-export async function updateArticle(req, res) {}
-export async function removeArticle(req, res) {}
-export async function createArticle(req, res) {}
+import Article from "../models/article.js";
+export async function getAllArticle(req, res) {
+  const articles = await Article.find({});
+  res.json({ message: "ok", data: articles });
+}
+export async function getArticle(req, res) {
+  const { id } = req.params;
+  const article = await Article.findOne({ _id: id });
+  res.json({ message: "ok", data: article });
+}
+export async function updateArticle(req, res) {
+  const { id } = req.params;
+  const { title, content, image } = req.body;
+  const update = { title, content, image };
+  const article = await Article.findOne({ _id: id });
+  if (!article) {
+    return res.status(400).json({ message: "Article Not Found", data: null });
+  }
+  const isDataChanged =
+    (title && article.title !== title) ||
+    (content && article.content !== content) ||
+    (image && article.image !== image);
+  if (!isDataChanged) {
+    return res.status(400).json({
+      message: "تغییراتی لحاظ نشده است , لطفا ابتدا مقادیر را تغییر دهید",
+      data: null,
+    });
+  }
+  const result = await Article.findByIdAndUpdate({ _id: id }, update, {
+    new: true,
+  });
+  res.json({ message: "ok", data: result });
+}
+export async function removeArticle(req, res) {
+  const { id } = req.params;
+  const result = await Article.deleteOne({ _id: id });
+  res.json({ message: "ok", data: result });
+}
+export async function createArticle(req, res) {
+  const { title, content } = req.body;
+  console.log(title, content);
+  //   const result = await Article.create({ title, content, image: "ssdadwqwq" });
+  //   res.json({ message: "ok", data: result });
+}
