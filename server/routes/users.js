e89dd0388fe13effd80626e52b5511a95b7fcca1
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2025-03-07 18:54:39 +0330

    to fix bug in authorize and auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,6 +10,7 @@ import {
   editUser,
 } from "../controllers/users.js";
 import { auth } from "../middelwares/auth.js";
+import { authorize } from "../middelwares/authorize.js";
 
 const router = express.Router();
 
@@ -17,8 +18,8 @@ router.post("/login", login);
 router.post("/signIn", signIn);
 router.post("/verifyOtp", verifyOTP);
 router.get("/me", auth, me);
-router.get("/allUsers", auth, users);
-router.get("/user/:id", auth, getUser);
-router.put("/user/:id", auth, editUser);
+router.get("/allUsers", auth, authorize(["admin"]), users);
+router.get("/user/:id", auth, authorize(["admin"]), getUser);
+router.put("/user/:id", auth, authorize(["admin"]), editUser);
 router.post("/editProfile", auth, editProfile);
 export default router;
